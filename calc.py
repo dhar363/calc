d478f5c4006b6b9bf7317a3244ecd34f1450af77
Dharvi <acoustic363@gmail.com>
2020-05-31 03:09:48 -0700

    Initial commit

---
+++ calc.py
@@ -0,0 +1,34 @@
+import re
+print("Calculator")
+print("To exit, type 'quit'\n")
+previous=0
+run=True
+
+def performMath():
+    global run
+    global previous
+    equation=''
+    if previous==0:
+        equation=input("Enter an equation:")
+    else:
+        equation=input(str(previous))
+
+    if equation=="quit":
+        run=False
+        print("Goodbye,human")
+    else:
+        equation=re.sub('[a-zA-z]','',equation)
+        if previous==0:
+            previous=eval(equation)
+        else:
+            previous=eval(str(previous)+equation)
+while run:
+    performMath()
+
+
+
+
+
+
+
+
